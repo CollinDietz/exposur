bef02f6a4be7857f38f6814861e7c5023ce9cbe6
Collin Dietz <c4dietz@gmail.com>
2020-04-05 19:02:34 -0400

    Add simple scroll list logic

--- lib/ActivityTab/ActivityTab.dart
+++ lib/ActivityTab/ActivityTab.dart
@@ -3,6 +3,28 @@ import 'package:flutter/material.dart';
 class ActivityTab extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Text('Activity');
+    return _showActivity();
+  }
+
+  Widget _showActivity() {
+    return ListView.builder(
+        padding: const EdgeInsets.all(16.0),
+        itemBuilder: (context, i) {
+          if (i.isOdd) return Divider(); /*2*/
+
+          // final index = i ~/ 2; /*3*/
+          // if (index >= _suggestions.length) {
+          //   _suggestions.addAll(generateWordPairs().take(10)); /*4*/
+          // } //handle needing more data
+          return _buildRow();
+        });
+  }
+
+  Widget _buildRow() {
+    return ListTile(
+      title: Text(
+        "Some Data",
+      ),
+    );
   }
 }
